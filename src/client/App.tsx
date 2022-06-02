268b12f23933a8b74274f8689fad36274371d45a
Viktor Popov <vitek.hoc@ya.ru>
2022-06-03 01:08:08 +0300

    Исправил структуру приложения. Теперь можно вести разработку клиентской части приложения.

--- src/client/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, {PureComponent} from 'react'
-import {render} from 'react-dom';
-
-const App = () => {
-
-    return <h1>Hello, World!</h1>;
-};
-
-render(<App />, document.getElementById('app'));
